3f4505f2f91a4cdb40b4267848c3b38938000f2c
proyirvin2333 <proyirvin2333@gmail.com>
2023-07-17 16:51:14 -0600

    Cambios

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyAmrWTfj6yeRvO1ikz83FM-K3XIvz7hoWw",
+    authDomain: "autic33iamc-a110c.firebaseapp.com",
+    projectId: "autic33iamc-a110c",
+    storageBucket: "autic33iamc-a110c.appspot.com",
+    messagingSenderId: "351979030759",
+    appId: "1:351979030759:web:e6637267ef348b41023a42",
+    measurementId: "G-GW8D525ZPV"
+});
